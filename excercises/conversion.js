a53c6124b26e3b4862875a40abd5f6b90214c383
m7exb <m77exb@gmail.com>
2024-04-24 15:59:06 -0400

    new changes

---
+++ excercises/conversion.js
@@ -0,0 +1,23 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+aResult = parseInt(a);
+console.log(aResult)
+
+bResult = parseInt(b);
+console.log(bResult)
+
+cResult = parseInt(c);
+console.log(cResult)
+
+dResult = parseInt(d);
+console.log(dResult)
+
+
+
